1e4ec6727b2b5df34526ada108fd87d12b7e6e4b
Earl Kennedy <mnenmenth@gmail.com>
2020-03-11 20:04:26 -0400

    Placeholder classes to create package folders

---
+++ src/main/java/io/github/virtualstocksim/controller/PlaceholderController.java
@@ -0,0 +1,9 @@
+package io.github.virtualstocksim.controller;
+
+/**
+ * Created by Earl Kennedy
+ * https://github.com/Mnenmenth
+ */
+public class PlaceholderController
+{
+}
---
+++ src/main/java/io/github/virtualstocksim/model/PlaceholderModel.java
@@ -0,0 +1,9 @@
+package io.github.virtualstocksim.model;
+
+/**
+ * Created by Earl Kennedy
+ * https://github.com/Mnenmenth
+ */
+public class PlaceholderModel
+{
+}
