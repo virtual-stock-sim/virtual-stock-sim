66bf61fdb9368d7b304f339e605af88dec639f34
Earl Kennedy <mnenmenth@gmail.com>
2020-03-22 14:29:36 -0400

    New StockDatabase class to replace StockCache in new database system

---
+++ src/main/java/io/github/virtualstocksim/stock/StockDatabase.java
@@ -0,0 +1,35 @@
+package io.github.virtualstocksim.stock;
+
+
+import io.github.virtualstocksim.database.DatabaseFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class StockDatabase
+{
+    private static final Logger logger = LoggerFactory.getLogger(StockData.class);
+    private final DataSource dataSource;
+    private static final String dbPath = DatabaseFactory.getConfig("stockdb.uri");
+
+    private static class StaticContainer
+    {
+        private static final StockDatabase Instance = new StockDatabase();
+    }
+
+    private static StockDatabase getInstance() { return StaticContainer.Instance; }
+
+    private StockDatabase()
+    {
+        dataSource = DatabaseFactory.getDatabase(dbPath);
+    }
+
+    private static Connection getConnection() throws SQLException
+    {
+        return getInstance().dataSource.getConnection();
+    }
+
+}
