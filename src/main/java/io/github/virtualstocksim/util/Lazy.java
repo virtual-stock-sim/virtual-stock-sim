866058f8da330778771e20b16893d3a0314d5141
Earl Kennedy <mnenmenth@gmail.com>
2020-03-22 14:30:46 -0400

    Rewrote Lazy.java to inherit apache commons lang LazyInitializer  - No need to reinvent the wheel, switching to established library instead

--- src/main/java/io/github/virtualstocksim/util/Lazy.java
+++ src/main/java/io/github/virtualstocksim/util/Lazy.java
@@ -1,38 +1,23 @@
 package io.github.virtualstocksim.util;
 
+import org.apache.commons.lang3.concurrent.ConcurrentException;
+import org.apache.commons.lang3.concurrent.LazyInitializer;
+
 import java.util.function.Supplier;
 
 // Container for an object to be lazily evaluated
-public class Lazy<T>
+public class Lazy<T> extends LazyInitializer<T>
 {
     private Supplier<T> supplier;
-    private T obj = null;
 
-    private Lazy(Supplier<T> supplier)
+    public Lazy(Supplier<T> supplier)
     {
         this.supplier = supplier;
     }
 
-    // Has the object been evaluated
-    public boolean hasEvaluated()
+    @Override
+    protected T initialize() throws ConcurrentException
     {
-        return obj != null;
-    }
-
-    // Evaluate the object and return
-        // If already evaluated, then return stored instance
-    public synchronized T get()
-    {
-        if(obj == null)
-        {
-            obj = supplier.get();
-        }
-        return obj;
-    }
-
-    // Lazy evaluation for lambda function
-    public static <T> Lazy<T> lazily(Supplier<T> toBeEval)
-    {
-        return new Lazy<>(toBeEval);
+        return supplier.get();
     }
 }
