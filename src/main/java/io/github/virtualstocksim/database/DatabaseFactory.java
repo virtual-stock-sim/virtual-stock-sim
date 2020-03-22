727f3f6e4f23249d8ff24de2234adf603c919bfa
Earl Kennedy <mnenmenth@gmail.com>
2020-03-22 16:38:00 -0400

    Fix static initialization in DatabaseFactory Add driver to URI in databases.config

--- src/main/java/io/github/virtualstocksim/database/DatabaseFactory.java
+++ src/main/java/io/github/virtualstocksim/database/DatabaseFactory.java
@@ -7,11 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
-import java.sql.SQLException;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,13 +17,15 @@ import java.util.concurrent.ConcurrentHashMap;
 public class DatabaseFactory
 {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseFactory.class);
-    private static Map<String, BasicDataSource> dataSources;
-    private static Properties config;
+    private Map<String, BasicDataSource> dataSources;
+    private Properties config;
 
     private static class StaticContainer
     {
         private static final DatabaseFactory Instance = new DatabaseFactory();
     }
+
+    private static DatabaseFactory getInstance() { return StaticContainer.Instance; }
 
     private DatabaseFactory()
     {
@@ -54,9 +54,9 @@ public class DatabaseFactory
     public static DataSource getDatabase(String databaseURI)
     {
         // Return existing data source for database if exists
-        if(!dataSources.containsKey(databaseURI))
+        if(getInstance().dataSources.containsKey(databaseURI))
         {
-            return dataSources.get(databaseURI);
+            return getInstance().dataSources.get(databaseURI);
         }
         // Else create and return a new data source
         else
@@ -64,14 +64,15 @@ public class DatabaseFactory
             BasicDataSource ds = new BasicDataSource();
             ds.setDriver(new EmbeddedDriver());
             ds.setUrl(databaseURI);
+            getInstance().dataSources.put(databaseURI, ds);
 
-            return dataSources.put(databaseURI, ds);
+            return getInstance().dataSources.get(databaseURI);
         }
     }
 
     public static String getConfig(String key)
     {
-        return config.getProperty(key);
+        return getInstance().config.getProperty(key);
     }
 
 }
